1dbd2ed2e718f47fb55d4c6521cb90f8f30c76ea
Othmane_Rhazlani <oghazlani0pgmail.com>
2021-07-08 21:07:22 +0100

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index(Request $request)
+    {
+        // if ($request->user()->role == 'admin')
+        // {
+        // // return redirect('/admin');
+        // return view('page.admin');
+        // }
+        // if ($request->user()->role == 'user')
+        // {
+        //     // return redirect('/user');
+        //     return view('page.user');
+        // }
+        // if ($request->user()->role == NULL)
+        // {
+        // dd('role is Null');
+        // }
+        // else{
+        //     dd('hello mother f**ker');
+        // }
+        
+        return view('home');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/user', [App\Http\Controllers\HomeController::class, 'index'])->middleware('redirectionUser');
+Route::get('/admin', [App\Http\Controllers\HomeController::class, 'index'])->middleware('redirectionUser');
+
+// Route::middleware(['redirectionUser'])->group(function () {
+//     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index']);  
+//     Route::get('/user', [App\Http\Controllers\UserController::class, 'index']);  
+//     Route::get('/admin', [App\Http\Controllers\AdminController::class, 'index']); 
+// });
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index']);
+Route::get('/user', [App\Http\Controllers\UserController::class, 'index']);
+Route::get('/admin', [App\Http\Controllers\AdminController::class, 'index']);
